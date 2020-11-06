8caf466275c2f7e359145f7788fb53ee4f757b75
Iurii Bairamukov <juribai@mail.ru>
2020-11-06 21:27:05 +0700

    add kata Shortest Word

---
+++ 7-ky/shortest-word.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+
+/* Shortest Word
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s) {
+  return Math.min(...s.split(' ').map((el) => el.length));
+}
