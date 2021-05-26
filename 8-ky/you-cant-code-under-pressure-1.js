1e3ce1eda7db83a4c58f417b24f48665a5cccfff
Iurii Bairamukov <juribai@mail.ru>
2021-05-26 22:33:52 +0700

    add kata You Can't Code Under Pressure #1

---
+++ 8-ky/you-cant-code-under-pressure-1.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+/* You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the integer and return it.
+
+*/
+
+function doubleInteger(i) {
+  return i * 2;
+}
